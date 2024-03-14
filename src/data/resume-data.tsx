5482be6ed11e8fb2b8c26fd6c85046d46cbd84ca
Zainab Lawal <zainablaw1012@gmail.com>
2024-03-14 19:55:49 +0300

    Feature: Add leadership experience and personal detail

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,143 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Zainab Damilola Lawal",
+  initials: "ZDL",
+  location: "Eskisehir, Turkey, IST",
+  locationLink: "https://www.google.com/maps/place/Eskisehir",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "A 3rd-year Computer Engineering major interested in Information Retrieval and NLP",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
+    `Highly motivated third-year undergraduate student with a strong interest in legal information retrieval (IR) and Natural Language Processing (NLP). Particularly interested in exploring the application of NLP techniques for enhancing access to legal information.` + `\nCurrently working on a personal project focused on improving the accuracy of Optical Character Recognition (OCR) results for Turkish legal documents through post-processing techniques.  Welcome to discuss this project further or any other aspects of my interests in legal IR and NLP.`,
+  avatarUrl: "https://avatars.githubusercontent.com/u/71593672?v=4",
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "zainablaw1012@gmail.com",
+    tel: "+905340407341",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/Zeesky-code",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://linkedin.com/in/zainabdlawal/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/Zeeskylaw",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Eskisehir Technical University",
+      degree: "Bachelor's Degree in Computer Engineering",
+      start: "2021",
+      end: "Present",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      company: "Kanunum",
+      link: "https://www.kanunum.com/",
+      badges: ["Remote", "Internship"],
+      title: "Software Engineering Intern",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      start: "07/2023",
+      end: "03/2024",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        ["Improved the accuracy of an algorithm for detecting names of parties, dates and case citations from legal documents.",
+        "Designed and developed microservices architecture leveraging Spring Boot, Tesseract, and Apache PDFBox to orchestrate and deploy advanced algorithms for detecting court judgments from scanned law journals.",
+        "Conducted research on information retrieval methods for Turkish legal content, focusing on legal information extraction and OCR enhancement techniques."
+        ],
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
+      company: "Library Aid Africa",
+      link: "https://www.libraryaidafrica.org/",
+      badges: ["Volunteering"],
+      title: "Software Engineering Intern",
       logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      start: "04/2023",
+      end: "11/2023",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      ["Spearheaded the development of RESTful APIs using the MERN (MongoDB, Express.js, React.js, Node.js) stack for the library user platform. ",
+      "Elevated code quality and reliability by implementing comprehensive unit testing strategies,resulting in a notable 40% increase in code coverage",
+      ],
     },
   ],
   skills: [
+    "HTML/ CSS",
     "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Node.js/ Express.js",
+    "Java",
+    "Springboot",
+    "MySQL",
+    "MongoDB",
+    "JDBC",
+    "Apache OpenNLP",
+    "ElasticSearch",
+    "Maven",
+    "Git",
+    "Tesseract"
   ],
   projects: [
     {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+      title: "Turkish Poetry Similarity Analyzer",
+      techStack: ["Java", "NLP", "Apache OpenNLP"],
+      description:
+        "Developed a Java application to analyze the semantic similarity between Turkish poems using NLP techniques. Focused on Nazim Hikmet's poems. Employed Apache OpenNLP for text pre-processing and stemming/lemmatization. Implemented cosine similarity for semantic closeness measurement.",
+      link: {
+        label: "See Project", 
+        href: "https://github.com/Zeesky-code/turkish-poetry-similarity", 
+      },
+    },
+    {
+      title: "Judgment Extractor (Turkish Legal Text)",
+      techStack: ["NLP", "Regular Expressions", "NER"],
+      description:
+        "Built a system to automatically extract Turkish court judgments from legal journals. Combined NLP techniques (text pre-processing, NER, keyword extraction) with regular expressions to identify judgments based on legal entities, keywords, and document structure.",
+      link: {
+        label: "Judgment Extractor", 
+        href: "https://github.com/Zeesky-code/pdf-extractor", 
+      },
+    },
+    {
+      title: "Swapify: Apple Music to Spotify Converter",
+      techStack: ["Express.js","Node.js","Web Scraping", "Spotify API"],
+      description:
+        "Developed 'Swapify', a web application that converts Apple Music links to their corresponding Spotify URLs. Utilized web scraping to extract song details and integrated with the Spotify API to search for matching songs and return Spotify links.",
+      link: {
+        label: "Swapify", 
+        href: "https://github.com/Zeesky-code/Swapify-API", 
+      },
+    },
+  ],
+  leadership: [
+    {
+      company:"Eskisehir Technical University",
+      badges: ["Teaching Assistantship"],
+      title: "Student Assistant- Database Management Systems",
+      start: "11/2023",
+      end: "present",
+      description:
+        ["Facilitate discussions, exercises, and group activities aimed at enhancing students' understanding and proficiency in database management concepts and technologies. ",
+        "Provide guidance and support for students working on the Database Management Systems term project, assisting with project requirements, implementation, and troubleshooting.",
+        ],
+    },
+    {
+      company:"ESTU Google Developers Student Club",
+      badges: ["Volunteering"],
+      title: "Core Team Organisation Member",
+      start: "10/2023",
+      end: "present",
+      description:
+      ["Organize workshops, hackathons, and events for the GDSC community. ",
+      "Organized DevFest Eskişehir 2023 with a turnout of 2k+ attendees.",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
